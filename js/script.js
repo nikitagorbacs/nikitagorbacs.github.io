3ffa7f0cb5a8a4f4ed1f63436b8fb6000467ab98
nikitagorbacs <80327407+nikitagorbacs@users.noreply.github.com>
2021-04-28 12:12:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,130 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+            breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+        $(this)
+          .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+          .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+      });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+                $('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+            })
+    
+        });
+    };
+    
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_buy').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalogue-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form){
+        $(form).validate( {
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please enter your name",
+                    minlength: jQuery.validator.format("At least {2} characters required!")
+                  },
+                phone: "Please enter your valid phone",
+                email: "Please enter the valid Email address"
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask(("+7 (999) 999-9999"));
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Smoth scropp and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    var wow = new WOW(
+        {
+          boxClass:     'wow',      // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          mobile:       true,       // trigger animations on mobile devices (default is true)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+          },
+          scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+      );
+      wow.init();
+  });
---
+++ js/test.js
@@ -0,0 +1,69 @@
+let name = "Niktia";
+let number = 7;
+let pi = 3.14;
+
+let leftBorderWidth = 200;
+
+//  number
+//  string - предметы, имена и прочее - "", '', ``
+//  true/false
+//  null - не существует в принципе
+// undefined - нет значения
+/* let object = {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+} */
+//  Symbol
+
+//  alert
+
+//  console.log(number);
+
+/* let answer = confirm("Вам есть 18?");
+console.log(answer) */
+
+/* let answer = prompt("Вам есть 18?");
+console.log(answer) */
+
+//  let isChecked = true,
+//      isClosed = false;
+
+//  console.log(isChecked && isClosed); - оператор И
+
+//  console.log(isChecked || isClosed); - оператор ИЛИ
+
+//if (2*4 == 4*1) {
+//    console.log('Correct!')
+//} else {
+//   console.log('Error!')
+//}
+
+//  let answer = confirm("Are you at least 18yo?")
+//  if (answer) {
+//      console.log('Come in')
+//  } else {
+//      console.log('Get the f out')
+//  }
+
+//  const num = 50;
+//  
+//  if (num < 49) {
+//      console.log('Its not enough')
+//  } else if (num > 51) {
+//      console.log('Its way too much')
+//  } else {
+//      console.log('Correct!')
+//  }
+
+//  for (let i = 1; i < 8; i++) {
+//      console.log(i);
+//  }
+
+function logging(a, b) {
+    console.log( a + b )
+}
+
+logging(3, 5);
+
+logging(2, 226);
